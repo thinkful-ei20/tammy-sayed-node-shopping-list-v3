f44b33fe0ef7df1b1ca462e3f184be92dda28f6b
Tammy Tian <tiantammy@outlook.com>
2018-04-19 00:08:31 +0800

    added delete recipes in server

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+'use strict';
 
 const express = require('express');
 const router = express.Router();
@@ -75,7 +76,16 @@ app.post('/recipes', jsonParser, (req, res) => {
 
 app.get('/recipes', (req, res) => {
   res.json(Recipes.get());
-})
+});
+
+app.delete('/recipes/:id', (req, res) => {
+  Recipes.delete(req.params.id);
+  console.log(`Deleted recipe item ${req.params.id}`);
+  res.status(204).end();
+});
+
+
+
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
